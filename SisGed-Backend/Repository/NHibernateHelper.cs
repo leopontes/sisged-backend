3a2634a41b2d8ce75d8d65fc9245c49073dc9000
Leonardo Lopes <leonardo.pontes.ter@estacio.br>
2020-03-28 16:19:10 -0300

    Ajustes

--- SisGed-Backend/Repository/NHibernateHelper.cs
+++ SisGed-Backend/Repository/NHibernateHelper.cs
@@ -15,13 +15,42 @@ namespace SisGed_Backend.Repository
         public static ISession OpenSession()
         {
             //string connectionStr = "postgresql://postgres:sucesso@2020@hostname:5432/postgres";
-            var connectionStr = "Server=127.0.0.1;Port=5432;Database=postgres;User Id=postgres;Password=sucesso@2020;";
-            var connectionStrHeroku = System.Environment.GetEnvironmentVariable("DATABASE_URL");
+            // var connectionStr = "Server=127.0.0.1;Port=5432;Database=postgres;User Id=postgres;Password=sucesso@2020;";
+            // var connectionStrHeroku = System.Environment.GetEnvironmentVariable("DATABASE_URL");
+            /*
+        postgres://
+        
+        //usuario
+        giadvikqhnkfsi:
+        //senha
+        1fe72e7dcf7ea2fadcc6c07060c6eb0dbd8fbacaf2afc97facac028a1ed1e461
+        //host
+        @ec2-34-206-252-187.compute-1.amazonaws.com:
+        //porta
+        5432
+        //banco        
+        d80o1da7m1q37u
+        */
+
+            Console.WriteLine("::::::::: Variaveis de ambiente ::::::::");
+            Console.WriteLine(System.Environment.GetEnvironmentVariable("USR"));
+            Console.WriteLine(System.Environment.GetEnvironmentVariable("PASSWORD"));
+            Console.WriteLine(System.Environment.GetEnvironmentVariable("HOST"));
+            Console.WriteLine(System.Environment.GetEnvironmentVariable("PORT"));
+            Console.WriteLine(System.Environment.GetEnvironmentVariable("DB"));
+            Console.WriteLine("::::::::: Variaveis de ambiente ::::::::");
 
             ISessionFactory sessionFactory = Fluently
              .Configure()
              .Database(
-                PostgreSQLConfiguration.Standard.ConnectionString(connectionStrHeroku)
+                PostgreSQLConfiguration.Standard.ConnectionString(conn=>
+                    conn
+                        .Username(System.Environment.GetEnvironmentVariable("USR", EnvironmentVariableTarget.Machine))
+                        .Password(System.Environment.GetEnvironmentVariable("PASSWORD", EnvironmentVariableTarget.Machine))
+                        .Host(System.Environment.GetEnvironmentVariable("HOST", EnvironmentVariableTarget.Machine))
+                        .Port(Int32.Parse(System.Environment.GetEnvironmentVariable("PORT", EnvironmentVariableTarget.Machine)))
+                        .Database(System.Environment.GetEnvironmentVariable("DB", EnvironmentVariableTarget.Machine))
+              )
                 .Dialect<PostgreSQL82Dialect>().ShowSql()
              )
              .Mappings(m => m.FluentMappings.AddFromAssemblyOf<Program>())
